d5ba6d85b86769d0afd5a06c3d8c45a0f6569685
georgejaniec <66422900+georgejaniec@users.noreply.github.com>
2023-05-04 20:57:07 +0000

    Some later additions

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+  return render_template('index.html')
+
+
+@app.route("/register")
+def register():
+  return render_template('register.html')
+
+
+@app.route("/login")
+def login():
+  return render_template('login.html')
+
+
+@app.route("/criteria")
+def criteria():
+  return render_template('criteria.html')
+
+
+@app.route("/gallery")
+def gallery():
+  return render_template('gallery.html')
+
+
+@app.route("/grasps")
+def grasps():
+  return render_template('grasps.html')
+
+
+@app.route("/samples")
+def samples():
+  return render_template('samples.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
